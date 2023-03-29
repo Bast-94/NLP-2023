ec5e8bb16599f20eda08d217594c22d6c0e342fa
François Soulier <francois.soulier@3ie.fr>
2023-03-29 18:03:20 +0200

    naive_bayes (Bonus): Finished integrating highest likelihood and stopwords removal

--- scripts/naive_bayes/scikit_learn.py
+++ scripts/naive_bayes/scikit_learn.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import numpy as np
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from nltk.corpus import stopwords
 
-def pipeline() -> Pipeline:
+def pipeline(stop_words: list[str] = []) -> Pipeline:
     """
     Returns a pipeline.
 
@@ -13,9 +16,34 @@ def pipeline() -> Pipeline:
         Pipeline: Scikit-learn pipeline
     """
     return Pipeline([
-        ("vect", CountVectorizer()),
+        ("vect", CountVectorizer(stop_words=stop_words)),
         ("clf", MultinomialNB())
     ])
+
+def print_most_likely_words(pipeline: Pipeline) -> None:
+    # Get the feature names.
+    feature_names: np.ndarray = pipeline.named_steps['vect'].get_feature_names_out()
+
+    # Get the log probabilities.
+    log_prob = pipeline.named_steps['clf'].feature_log_prob_
+
+    # Get the index of the most likely words.
+    most_likely = np.argsort(log_prob, axis=1)[:, -10:]
+
+    # Print the most likely words.
+    for i in range(2):
+        print("Class: ", i)
+        print("Most likely words: ", [feature_names[j] for j in most_likely[i]])
+        print("")
+
+def get_stopwords() -> None:
+    """
+    Returns a list of stopwords.
+
+    Returns:
+        list: List of stopwords
+    """
+    return stopwords.words("english")
 
 #-----------------------------#
 #       Evaluation            #
